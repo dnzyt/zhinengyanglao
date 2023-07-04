351194d3ae58b7d06f840d33f788179aa593648f
dnzyt <dnzyt1986@gmail.com>
2023-07-03 19:58:51 -0700

    add dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,20 @@ export default function App() {
 
         <div className="relative h-11 py-1 mb-3 border-b-2">
           <nav className="flex flex-nowrap justify-around py-1 text-center">
-            <Link className="px-0 text-lg" to="yiyang">医养服务</Link>
+            <div className="relative">
+              <button className="peer px-0 text-lg" type="button">医养服务</button>
+              <div className="absolute left-0 top-[calc(100%+.5rem)] w-48 p-1 rounded shadow-xl opacity-0 bg-white
+              transition-all duration-250 ease-in-out -translate-y-2 peer-focus:opacity-100 peer-focus:translate-y-0">
+                <ul className="list-none">
+                  <li className="py-1 hover:bg-sky-100"><Link className="block w-full text-lg" to="yiyang">医养服务</Link></li>
+                  <li className="py-1 hover:bg-sky-100"><Link className="block w-full text-lg" to="kangyang">康养服务</Link></li>
+                  <li className="py-1 hover:bg-sky-100"><Link className="block w-full text-lg" to="zhuji">助急服务</Link></li>
+                  <li className="py-1 hover:bg-sky-100"><Link className="block w-full text-lg" to="renli">人力资源</Link></li>
+                  <li className="py-1 hover:bg-sky-100"><Link className="block w-full text-lg" to="shuangchuang">双创服务</Link></li>
+                  <li className="py-1 hover:bg-sky-100"><Link className="block w-full text-lg" to="lvyou">旅游服务</Link></li>
+                </ul>
+              </div>
+            </div>
             <Link className="px-0 text-lg" to="kangyang">康养服务</Link>
             <Link className="px-0 text-lg" to="zhuji">助急服务</Link>
             <Link className="px-0 text-lg" to="renli">人力资源</Link>
